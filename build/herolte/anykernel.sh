e7314b59b169b4512259c6fca50fc75a124ffeb8
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-06-20 00:05:47 +0200

    build: add AnyKernel2 (05cb011) adapted to exynos8890
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

---
+++ build/herolte/anykernel.sh
@@ -0,0 +1,46 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=Primal Kernel by kylothow @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=herolte
+device.name2=heroltebmc
+device.name3=herolteskt
+device.name4=heroltektt
+device.name5=heroltelgt
+} # end properties
+
+# shell variables
+block=/dev/block/platform/155a0000.ufs/by-name/BOOT;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+# permission settings will be added here
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# ramdisk changes will be added here
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
