860f85a61b19b4bda28bdd31ece9cab44b15ff0d
Michele Beccalossi <beccalossi.michele@gmail.com>
2017-07-06 14:52:45 +0200

    build_all.sh: add simple wrapper to batch build for all the targets
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

---
+++ build_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 Michele Beccalossi <beccalossi.michele@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+./build.sh 0
+./build.sh 1
+./build.sh 0
+./build.sh 2
+./build.sh 0
+./build.sh 3
+./build.sh 0
+./build.sh 4
